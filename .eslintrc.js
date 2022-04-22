32c6003422e5c0a2ba5454dba775a70f2367c64b
Tomás Sánchez Martínez <tomassanchezmartinez@MacBook-Pro-de-Tomas.local>
2022-04-22 21:04:40 +0200

    Added Eslint,jest,sass,husky dependences

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+
+  extends: ["airbnb-base", "prettier"],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+
+    sourceType: "module",
+  },
+
+  rules: { "no-plusplus": "off", "no-restricted-syntax": "off" },
+
+  "lines-between-class-members": [
+    "error",
+
+    "always",
+
+    { exceptAfterSingleLine: true },
+  ],
+
+  babel: {
+    env: {
+      test: {
+        plugins: ["@babel/plugin-transform-modules-commonjs"],
+      },
+    },
+  },
+
+  jest: {
+    testEnvironment: "jsdom",
+  },
+};
